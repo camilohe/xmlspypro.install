3a750f0ae2073c424c54bb3f3e9064faeedd532a
estevc <camilo.estevez@md.ca>
2019-11-25 12:59:51 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+# xmlspypro.install
+A [chocolatey](https://chocolatey.org/) [**package**](https://chocolatey.org/packages/xmlspypro.install) to install [**Altova XML Spy Professional**](http://www.altova.com/xmlspy.html) on Windows.
+
+**Note:** The software *must* be activated before use. You can request a free 30 day trial key from the activation wizard.
+
+## changes
+- initial version: xmlspypro 2019.3.1
---
+++ tools/ChocolateyInstall.ps1
@@ -0,0 +1,41 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'xmlspypro'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://cdn.sw.altova.com/v2019r3sp1/en/XMLSpyProf2019rel3sp1.exe'
+$checksum   = 'CBCD3F14EC45E2656B8E56F2B5FCD45078533741288176E37A6536FA81D7F870'
+$url64      = 'https://cdn.sw.altova.com/v2019r3sp1/en/XMLSpyProf2019rel3sp1_x64.exe'
+$checksum64 = 'F271297C4AB2E90F9564F9BE04DCD5B0CB321BEA16BC651043F36FE583059C51'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = "/qn /l*v `"$env:TEMP\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'xmlspy*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+}
+
+
+# HACK: The XMLSpy installer fails when this key is already present.
+$key = "MenuExt"
+$path = "HKCU:\SOFTWARE\Microsoft\Internet Explorer\$key"
+Rename-Item -Path $path  -NewName "$key BAK" -ea SilentlyContinue
+
+Try {
+	Install-ChocolateyPackage @packageArgs
+}
+Finally {
+	# Restore the hack.
+	Remove-Item -Path $path -Recurse -ea SilentlyContinue
+	Rename-Item -Path "$path BAK"  -NewName $key -ea SilentlyContinue
+}
